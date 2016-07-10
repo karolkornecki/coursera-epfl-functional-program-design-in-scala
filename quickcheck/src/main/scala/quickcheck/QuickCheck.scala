b428bb2536a24c499499e85b9622aab3fb70ffa0
karolkornecki <karol.kornecki@gmail.com>
2016-07-10 09:56:59 +0200

    week 3 - quickcheck

--- quickcheck/src/main/scala/quickcheck/QuickCheck.scala
+++ quickcheck/src/main/scala/quickcheck/QuickCheck.scala
@@ -19,25 +19,41 @@ abstract class QuickCheckHeap extends Properties("Heap") with IntHeap {
     findMin(insert(m, h)) == m
   }
 
+  // hint 1
   property("If you insert any two elements into an empty heap, finding the minimum of the resulting heap should get the smallest of the two elements back.") =
     forAll { (n1: A, n2: A) =>
       findMin(insert(n2, insert(n1, empty))) == Math.min(n1, n2)
     }
 
+  // hint 2
   property("If you insert an element into an empty heap, then delete the minimum, the resulting heap should be empty.") =
     forAll { (n1: A) =>
       isEmpty(deleteMin(insert(n1, empty)))
     }
 
+  // hint 3
   property("Given any heap, you should get a sorted sequence of elements when continually finding and deleting minima.") =
     forAll { (h: H) =>
       isSorted(h)
     }
 
+  // hint 4
   property("Finding a minimum of the melding of any two heaps should return a minimum of one or the other.") =
     forAll { (n1: A, n2: A, n3: A, n4: A) =>
       findMin(meld(insert(n2, insert(n1, empty)), insert(n4, insert(n3, empty)))) == Math.min(n1, Math.min(n2, Math.min(n3, n4)))
     }
+
+  // one heap create from merging two heap is equal to another heap created from merging such two heap that one is created by removing min
+  // element which is added to the second one.
+  property("Two heap should be equal when removing min element recursively until both heaps become empty.") = forAll { (h1: H, h2: H) =>
+
+    def isEqual(h1: H, h2: H): Boolean =
+      if (isEmpty(h1) && isEmpty(h2)) true
+      else findMin(h1) == findMin(h2) && isEqual(deleteMin(h1), deleteMin(h2))
+
+    isEqual(meld(h1, h2), meld(deleteMin(h1), insert(findMin(h1), h2)))
+
+  }
 
   def isSorted(h: H): Boolean = {
     if (isEmpty(h)) true
